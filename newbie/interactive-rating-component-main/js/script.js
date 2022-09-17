13bbb595e6caf4f6ad221d597d83f6931188584a
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2022-09-16 22:09:25 -0600

    add[id]: listed items

--- newbie/interactive-rating-component-main/js/script.js
+++ newbie/interactive-rating-component-main/js/script.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {});
+const ratingChecked = () => {
+  let numberOne = parseInt(document.querySelector("#1").value);
+  if (numberOne === 1) {
+    console.log("You cliked numer one");
+  } else {
+    console.log("Nothing detected");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  let numberOne = document.getElementById("1");
+  if (numberOne == 1) {
+    console.log("You cliked numer one");
+  } else {
+    console.log("Nothing detected");
+  }
+});

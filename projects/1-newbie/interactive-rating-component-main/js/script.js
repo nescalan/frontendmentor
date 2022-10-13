70786aa9746f50dd306444742fe9074924aa70d4
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-10-13 11:37:26 -0600

    update: card styles

--- projects/1-newbie/interactive-rating-component-main/js/script.js
+++ projects/1-newbie/interactive-rating-component-main/js/script.js
@@ -1,17 +1,5 @@
-const ratingChecked = () => {
-  let numberOne = parseInt(document.querySelector("#1").value);
-  if (numberOne === 1) {
-    console.log("You cliked numer one");
-  } else {
-    console.log("Nothing detected");
-  }
-};
-
 document.addEventListener("DOMContentLoaded", function () {
-  let numberOne = document.getElementById("1");
-  if (numberOne == 1) {
-    console.log("You cliked numer one");
-  } else {
-    console.log("Nothing detected");
-  }
+  document.getElementById("1").addEventListener("click", function () {
+    console.log("pulso el uno");
+  });
 });
